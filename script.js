743f0b3fd8bfdefb2af704254d731f7cd2d67a77
aiyyer <gadavid@calpoly.edu>
2022-08-02 14:20:27 -0700

    console tests for file usage

---
+++ script.js
@@ -0,0 +1,9 @@
+const fileInput = document.getElementById("input");
+inputElement.addEventListener("change", handleFile, false);
+
+function handleFile() {
+	console.log(files);
+	console.log(file);
+}
+
+
