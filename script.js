459e7d6c44259b984dc61bd4c08ba3ed831a6226
aiyyer <gadavid@calpoly.edu>
2022-08-02 14:56:20 -0700

    testing new file reading code

---
+++ script.js
@@ -0,0 +1,13 @@
+const fileInput = document.getElementById("file");
+fileInput.addEventListener("change", handleFile, false);
+
+function handleFile(event) {
+	const reader = new FileReader();
+	reader.onload = (function(evt) {
+		console.log(evt);
+		document.getElementById('fileContent').textContent = evt.target.result;
+	})
+	reader.readAsText(event.target.files[0]);
+}
+
+
