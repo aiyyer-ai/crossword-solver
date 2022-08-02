0d22d2446da1ec510ab5245b6f559262b2875041
aiyyer <gadavid@calpoly.edu>
2022-08-02 15:03:58 -0700

    Revert "console log for error finding"
    This reverts commit 251d3e8f60532891ff1992f955f77d29771b2512.

--- script.js
+++ script.js
@@ -7,8 +7,7 @@ function handleFile(event) {
 		console.log(evt);
 		document.getElementById('fileContent').textContent = evt.target.result;
 	})
-	console.log(event.target);
-	reader.readAsText(event.target.file);
+	reader.readAsText(event.target.files[0]);
 }
 
 
