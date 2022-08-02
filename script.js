d64e9e694bb7d6fa658295075f92ba83ee9e41a3
aiyyer <gadavid@calpoly.edu>
2022-08-02 15:10:23 -0700

    bug finding

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ fileInput.addEventListener("change", handleFile, false);
 function handleFile(event) {
 	const reader = new FileReader();
 	reader.onload = (function(evt) {
-		document.getElementById('fileContent').textContent = evt.target.result;
+		evt.target.result;
 	})
-	reader.readAsText(event.target.file);
+	const file = event.target.file;
+	console.log(file);
 }
