56a998799dd1c7f046af8648c2d401384c40c61c
aiyyer <gadavid@calpoly.edu>
2022-08-03 15:08:37 -0700

    a

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ function createBoard(info) {
 	render.backgroundColor = 0x000000;
     render.view.style.position = "absolute";
     render.view.style.display = "block";
-    render.view.style.width = `${(boardWidth * 36) + 2}px`;
-    render.view.style.height = `${(boardHeight * 36) + 2}px`;
+    //render.view.style.width = `${(boardWidth * 36) + 2}px`;
+    //render.view.style.height = `${(boardHeight * 36) + 2}px`;
     render.autoResize = true;
-    //render.resize((boardWidth * 36) + 2, (boardHeight * 36) + 2);
+    render.resize((boardWidth * 36) + 2, (boardHeight * 36) + 2);
     app.render();
     const inputField = document.getElementById("input");
 	document.body.insertBefore(app.view, inputField);
