a0e74ce447d99ab44ad12deb0ae1e7a0495c3402
aiyyer <gadavid@calpoly.edu>
2022-08-03 02:12:11 -0700

    test log

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function createBoard(info) {
 			crosswordSquare.interactive = true;
 			crosswordSquare.name = `${squarePosition},${row}`
 			crosswordSquare.on('click', (event) => onClick(crosswordSquare));
-			keyboard.events.on('pressed', null, (keyCode, event) => keyPress(keyCode));
+			keyboard.events.on('pressed', null, (keyCode, event) => { console.log(keyCode); });
 			squarePosition++;
 			app.stage.addChild(crosswordSquare);
 			if(square != 0 && typeof square == 'number') {
