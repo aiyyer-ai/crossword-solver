eed119c45f21141ddded865545686a509d61999d
aiyyer <gadavid@calpoly.edu>
2022-08-04 13:20:29 -0700

    more test scrolls

--- script.js
+++ script.js
@@ -24,12 +24,12 @@ function createBoard(info) {
 	console.log(info);
 	boardWidth = info.dimensions.width;
 	boardHeight = info.dimensions.height;
-	app = new PIXI.Application({ width: (boardWidth * 36) + 2 + 502, height: (boardHeight * 36) + 2, resolution: 4, antialias: true })
+	app = new PIXI.Application({ width: (boardWidth * 36) + 2, height: (boardHeight * 36) + 2, resolution: 4, antialias: true })
 	let render = app.renderer;
 	render.backgroundColor = 0x000000;
     render.view.style.position = "absolute";
     render.view.style.display = "block";
-    render.view.style.width = `${(boardWidth * 36) + 2 + 502}px`;
+    render.view.style.width = `${(boardWidth * 36) + 2}px`;
     render.view.style.height = `${(boardHeight * 36) + 2}px`;
     render.autoResize = true;
     app.render();
@@ -73,18 +73,18 @@ function createBoard(info) {
 			}
 		}
 	}
-    let clueContainer = new PIXI.Container();
-    app.stage.addChild(clueContainer);
-    clueContainer.x = (boardWidth * 36) + 2;
-    clueContainer.y = 2;
-    clueContainer.height = (boardHeight * 36) + 2;
-	clueContainer.width = 500;
-	let clueArea = new PIXI.Graphics();
-	clueArea.beginFill(0xffffff);
-	clueArea.drawRect(0, 0, 500, (boardHeight * 36) - 2);
-	clueArea.beginFill(0x333333);
-	clueArea.drawRect(249, 0, 2, (boardHeight * 36) - 2);
-	clueContainer.addChild(clueArea);
+ //    let clueContainer = new PIXI.Container();
+ //    app.stage.addChild(clueContainer);
+ //    clueContainer.x = (boardWidth * 36) + 2;
+ //    clueContainer.y = 2;
+ //    clueContainer.height = (boardHeight * 36) + 2;
+	// clueContainer.width = 500;
+	// let clueArea = new PIXI.Graphics();
+	// clueArea.beginFill(0xffffff);
+	// clueArea.drawRect(0, 0, 500, (boardHeight * 36) - 2);
+	// clueArea.beginFill(0x333333);
+	// clueArea.drawRect(249, 0, 2, (boardHeight * 36) - 2);
+	// clueContainer.addChild(clueArea);
 }
 
 function findWordStart(position) {
