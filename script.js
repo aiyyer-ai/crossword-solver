aacf21ddc13b4ff5a037df8a3160d3d2f34a75af
aiyyer <gadavid@calpoly.edu>
2022-08-05 15:07:36 -0700

    change pointer capture to across

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ function onScrollbarClick(scrollbutton, event) {
 function onScrollClick(scrollbutton, event) {
 	scrollbutton.tint = 0x616161;
 	scrollbutton.heightDifference = scrollbutton.y - event.data.global.y;
-	scrollbutton.setPointerCapture(event.pointerId);
+	across.setPointerCapture(event.pointerId);
 	scrollbutton.dragging = true;
 }
 
@@ -233,7 +233,7 @@ function offScrollClick(scrollbutton, event) {
 	if(scrollbutton.dragging) {
 		scrollbutton.tint = 0xffffff;
 		scrollbutton.heightDifference = 0;
-		scrollbutton.releasePointerCapture(event.pointerId);
+		across.releasePointerCapture(event.pointerId);
 		scrollbutton.dragging = false;
 	}
 }
