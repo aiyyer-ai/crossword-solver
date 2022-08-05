a3b119b76ee734c0d5235591b87c326b17e9c0b5
aiyyer <gadavid@calpoly.edu>
2022-08-05 15:16:25 -0700

    document body pointer capture

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ function onScrollbarClick(scrollbutton, event) {
 function onScrollClick(scrollbutton, event) {
 	scrollbutton.tint = 0x616161;
 	scrollbutton.heightDifference = scrollbutton.y - event.data.global.y;
-	document.setPointerCapture(event.pointerId);
+	document.body.setPointerCapture(event.pointerId);
 	scrollbutton.dragging = true;
 }
 
@@ -233,7 +233,7 @@ function offScrollClick(scrollbutton, event) {
 	if(scrollbutton.dragging) {
 		scrollbutton.tint = 0xffffff;
 		scrollbutton.heightDifference = 0;
-		document.releasePointerCapture(event.pointerId);
+		document.body.releasePointerCapture(event.pointerId);
 		scrollbutton.dragging = false;
 	}
 }
