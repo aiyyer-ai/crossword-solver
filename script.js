e9537c3b98faa5d7dec6f5ebb374c0b5e3c17591
aiyyer <gadavid@calpoly.edu>
2022-08-05 12:50:34 -0700

    document body mouse up

--- script.js
+++ script.js
@@ -181,7 +181,7 @@ function createBoard(info) {
 	scrollbutton.on('pointerover', (event) => onScrollOver(scrollbutton));
 	scrollbutton.on('mousedown', (event) => onScrollClick(scrollbutton, event));
 	scrollbutton.on('mousemove', (event) => onScrollDrag(scrollbutton, event));
-	scrollbutton.on('mouseup', (event) => offScrollClick(scrollbutton, event));
+	document.body.on('mouseup', (event) => offScrollClick(scrollbutton, event));
 	scrollbutton.on('pointerout', (event) => offScrollOver(scrollbutton));
 	scrollbarContainer.addChild(scrollbutton);
 	//I'll need to add more events
@@ -211,9 +211,9 @@ function onScrollClick(scrollbutton, event) {
 
 function onScrollDrag(scrollbutton, event) {
 	if(scrollbutton.dragging) {
-		if(Math.abs(scrollbutton.x - event.data.global.x) > 400) {
-			offScrollClick(scrollbutton, event);
-		}
+		// if(Math.abs(scrollbutton.x - event.data.global.x) > 400) {
+		// 	offScrollClick(scrollbutton, event);
+		// }
 		let scrollbuttonRect = scrollbutton.getLocalBounds();
 		if((event.data.global.y + scrollbutton.heightDifference) > 0 && (event.data.global.y + scrollbutton.heightDifference + scrollbuttonRect.height) < ((boardHeight * 36) + 2)) {
 			scrollbutton.y = event.data.global.y + scrollbutton.heightDifference;
