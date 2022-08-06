dc3b4de3723cfec64e1ee2ba562633057fb4a539
aiyyer <gadavid@calpoly.edu>
2022-08-06 01:53:48 -0700

    proper pathing

--- script.js
+++ script.js
@@ -580,8 +580,8 @@ function onClueClick(object) {
 	} else {
 		currentHighlight.across = true;
 	}
-	console.log(object.parent.squares[0]);
-	let randomSquare = object.parent.squares[0].name.split(",");
+
+	let randomSquare = object.parent.squares[0].squareData.name.split(",");
 	let firstSquare = findClueNum(pickaname, object.dir, true);
 	setHighlight(firstSquare);
 
