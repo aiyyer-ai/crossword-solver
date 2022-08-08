794fd436fd58dc5e507eec5ef7c5f73ebe853629
aiyyer <gadavid@calpoly.edu>
2022-08-07 20:25:50 -0700

    update

--- script.js
+++ script.js
@@ -679,8 +679,7 @@ function findSpot(clickedPos, insert) {
 	} else {
 		spotCheck = currentHighlight.across ? [clickedPos[0] - 1, clickedPos[1]] : [clickedPos[0], clickedPos[1] - 1];
 	}
-	console.log(spotCheck);
-	let newSpot = allSquares.getChildByName(spotCheck).children[0];
+	let newSpot = allSquares.getChildByName(spotCheck.toString()).children[0];
 	if(newSpot) {
 		if(checkedCorrect.indexOf(spotCheck.toString()) == -1) {
 			return setHighlight(newSpot);
